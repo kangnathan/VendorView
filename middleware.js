d76eb6cd04d160ee997f813370b8ec6d0d2d5285
kangnathan <C1206-23@itmlyceumalabang.onmicrosoft.com>
2025-01-05 19:18:36 +0800

    1/5/25-3

--- middleware.js
+++ middleware.js
@@ -2,25 +2,29 @@ import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 import * as jose from 'jose'
 
-const jwtSecret = process.env.JWTSECRET
+const jwtSecret = process.env.JWT_SECRET
 
 export async function middleware(request) {
-  const cookieStore = await cookies()
-  const cookie = cookieStore.get('vendor-view')
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get('vendor-view');
 
   if (!cookie) {
-    return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.redirect(
+      new URL('/', request.url)
+    );
   }
 
   try {
-    const decoded = jose.decodeJwt(cookie.value)
-    await jose.jwtVerify(cookie.value, new TextEncoder().encode(jwtSecret))
+    const decoded = jose.decodeJwt(cookie.value);
+    
+    await jose.jwtVerify(cookie.value, new TextEncoder().encode(jwtSecret));
 
-    const userId = decoded.userId
+    const userId = decoded.userId;  
 
     if (!userId) {
       return NextResponse.redirect(new URL('/', request.url))
     }
+
   } catch (error) {
     return NextResponse.redirect(new URL('/', request.url))
   }
@@ -39,6 +43,6 @@ export const config = {
     '/product-edit-page/',
     '/supplier-page',
     '/location',
-    '/security'
+    '/security',
   ]
 }
